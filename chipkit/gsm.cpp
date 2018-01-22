e459d722bb267801f561c95635b5dd96fb101ce5
fpimenta <franciscofernandespimenta@gmail.com>
2018-01-22 00:02:23 +0000

    Update gsm.cpp

--- chipkit/gsm.cpp
+++ chipkit/gsm.cpp
@@ -1,108 +1,111 @@
+/*
+
+
+*/
+
 #include <TinyGPS++.h>
 
-
-#define SIM900 Serial1
-
- TinyGPSPlus gps;
-char respuesta[120];
- 
-
+TinyGPSPlus gps; // This library parses NMEA data
+char answer[120];
  
 void setup() {
-   Serial1.begin(9600);
+ Serial1.begin(9600);
  Serial.begin(9600);
  delay(100);
-  enviarAT("AT", "OK", 1000,0); // Cuelga la llamada
-  enviarAT("AT+CPIN?", "OK", 1000,0);
-  while ( enviarAT("AT+CREG?", "+CREG: 0,1", 1000,0) == 0 )
+ pinMode(2,OUTPUT);
+ pinMode(3,OUTPUT);
+ sendAT("AT", "OK", 1000,0); // Check connection
+ sendAT("AT+CPIN?", "OK", 1000,0); // Unlock SIM
+  while ( sendAT("AT+CREG?", "+CREG: 0,1", 1000,0) == 0 ) // Loops until it finds gsm signal
   {
     delay(100);
   }
-  Serial.println("Conectado a la red.");
-  enviarAT("AT+CLIP=1\r", "OK", 1000,0); // Activamos la identificacion de llamadas
-  enviarAT("AT+CMGF=1\r", "OK", 1000,0); //Configura el modo texto para enviar o recibir mensajes
-  enviarAT("AT+CGPSPWR=1\r", "OK", 1000,0);
+  Serial.println("Connected.");
+  sendAT("AT+CLIP=1\r", "OK", 1000,0); // Activate call identification
+  sendAT("AT+CMGF=1\r", "OK", 1000,0); // Activate SMS
+  sendAT("AT+CGPSPWR=1\r", "OK", 1000,0); // Power up the GPS
+  digitalWrite(3,HIGH); // Ready to make calls and send messages
 }
  
 void loop() {
   char r = 0;
-  //Envíamos y recibimos datos
+ // Read a char from the Serial monitor
  if (Serial.available() > 0){
   r = Serial.read();
  }
- if(r == 'c'){
-  Serial.println("chamada");
-  enviarAT("ATD968174036;", "OK", 1000,0);
+ if(r == 'c' || digitalRead(29) == HIGH){ // Make a call. The number is hardcoded for simplification
+  Serial.println("Making a call");
+  sendAT("ATD968174036;", "OK", 1000,0);
  }
- if(r == 's'){
-  char sms[60] ; //= "es coco\x1A \r\n";
+ if(r == 's'  || digitalRead(30) == HIGH){ // Send a message with the position data
+  char sms[60] ; 
   sprintf(sms, "Lat: %f\nLon: %f\nAlt:%f\x1A \r\n", gps.location.lat(),gps.location.lng(),gps.altitude.meters());
   char aux_str[120];
-  Serial.println("Enviando SMS...");
-    enviarAT("AT+CMGF=1\r", "OK", 1000,0); //Comando AT para mandar un SMS
-    sprintf(aux_str, "AT+CMGS=\"968174036\"", strlen(sms)); //Numero al que vamos a enviar el mensaje
-    //Texto del mensaje
-    if (enviarAT(aux_str, ">", 10000,0) == 1)
+  Serial.println("Sending SMS...");
+  //sendAT("AT+CMGF=1\r", "OK", 1000,0); 
+  sprintf(aux_str, "AT+CMGS=\"968174036\"", strlen(sms)); // Number to send SMS
+    //SMS text
+    if (sendAT(aux_str, ">", 10000,0) == 1)
     {
-      enviarAT(sms, "OK", 10000,0);
+      sendAT(sms, "OK", 10000,0);
     }
-    Serial.println("SMS enviado");
+    Serial.println("SMS sent");
    
  }
- if(r == 'g'){
+ if(r == 'g' || digitalRead(31) == HIGH){ // Loops until it finds gps signal, after that it just updates the data
     Serial.println("Finding GPS");
-    while ( enviarAT("AT+CGPSSTATUS?", "+CGPSSTATUS: Location 3D Fix", 1000,0) == 0 )
-  {
+    digitalWrite(2, HIGH);
+    while ( sendAT("AT+CGPSSTATUS?", "+CGPSSTATUS: Location 3D Fix", 1000,0) == 0 ) // It usually takes a lot to find signal
+    {
     delay(5000);
-  }
-  
-  //while ( SIM900.available() != 0) SIM900.read();
-  enviarAT("AT+CGPSOUT\r", "OK",1000,1);
+    }
+    sendAT("AT+CGPSOUT\r", "OK",1000,1); // Asks for position data in NMEA format
   
  
-  Serial.println(gps.location.lat(),6);
-  Serial.println(gps.location.lng(), 6);
-  Serial.println(gps.altitude.meters());
-  Serial.println(gps.speed.kmph());
+    Serial.println(gps.location.lat(),6);
+    Serial.println(gps.location.lng(), 6);
+    Serial.println(gps.altitude.meters());
+    Serial.println(gps.speed.kmph());
+    delay(1000);
+    digitalWrite(2,LOW);
  }
  
 }
- 
- 
-int enviarAT(String ATcommand, char* resp_correcta, unsigned int tiempo, int loc)
+  
+int sendAT(String ATcommand, char* return_code, unsigned int time, int loc)
 {
  
   int x = 0;
-  bool correcto = 0;
+  bool b = 0;
  
-  unsigned long anterior;
+  unsigned long old;
  
-  memset(respuesta, '\0', 100); // Inicializa el string
+  memset(answer, '\0', 100); // Inicializa el string
   delay(100);
-  while ( SIM900.available() > 0) SIM900.read(); // Limpia el buffer de entrada
-  SIM900.println(ATcommand); // Envia el comando AT
+  while ( Serial1.available() > 0) Serial1.read(); // Limpia el buffer de entrada
+  Serial1.println(ATcommand); // Envia el comando AT
   x = 0;
-  anterior = millis();
+  old = millis();
   // Espera una respuesta
   do {
     // si hay datos el buffer de entrada del UART lee y comprueba la respuesta
-    if (SIM900.available() != 0)
+    if (Serial1.available() != 0)
     {
-       respuesta[x] = SIM900.read();
+       answer[x] = Serial1.read();
        if(loc)
-       gps.encode(respuesta[x]);
-        x++;
+       gps.encode(answer[x]);
+       x++;
       // Comprueba si la respuesta es correcta
-      if (strstr(respuesta, resp_correcta) != NULL)
+      if (strstr(answer, return_code) != NULL)
       {
-        correcto = 1;
+        b = 1;
       }
     }
   }
   // Espera hasta tener una respuesta
-  while ((correcto == 0) && ((millis() - anterior) < tiempo));
-  Serial.println(respuesta);
+  while ((b == 0) && ((millis() - old) < time));
+  Serial.println(answer);
  
-  return correcto;
+  return b;
  
   }
